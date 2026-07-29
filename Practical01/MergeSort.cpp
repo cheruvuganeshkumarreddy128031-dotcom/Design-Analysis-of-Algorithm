b609ca49d8fdb650042e51efacd054c2783d8612
cheruvuganeshkumarreddy128031-dotcom <cheruvu.ganeshkumarreddy128031@marwadiuniversity.ac.in>
2026-07-29 23:46:02 +0530

    Update MergeSort.cpp

--- Practical01/MergeSort.cpp
+++ Practical01/MergeSort.cpp
@@ -1,35 +1,69 @@
-//insertion sort
-#include<iostream>
-using namespace std;
+#include<stdio.h>
+void merge(int arr[], int left, int mid, int right)
+{
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+    int temp[100];
+    while(i <= mid && j <= right)
+    {
+        if(arr[i]<arr[j])
+        {
+            temp[k]=arr[i];
+            i++;
+        }
+        else
+        {
+            temp[k]=arr[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<=mid)
+    {
+        temp[k]=arr[i];
+        i++;
+        k++;
+    }
+    while(j<=right)
+    {
+        temp[k]=arr[j];
+        j++;
+        k++;
+    }
+    for(int i=left;i<=right;i++)
+    {
+        arr[i]=temp[i];
+    }
+}
+void mergeSort(int arr[], int left, int right)
+{
+    if(left < right)
+    {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(int arr[], int size)
+ {
+    for (int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
-    int n, temp, j;
-    printf("Enter the no of elements :");
-    cin>>n;
-
-    int a[n];
-    printf("Enter the elements :");
-    for(int i=0;i<n;i++)
-    {
-        cin>>a[i];
-    }
-
-    for(int i=1;i<n;i++){
-        temp = a[i];
-        j = i-1;
-
-        while(j>=0 && a[j]>temp){
-            a[j+1]=a[j];
-            j--;
-        }
-        a[j+1]=temp;
-    }
-
-
-    cout<<"Sorted array - ";
-    for(int i=0;i<n;i++)
-    {
-        printf("%d ", a[i]);
-    }
-
-} 
+    int arr[10], n, i;
+    printf("Enter the number of array elements:\n");
+    scanf("%d", &n);
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++)
+    scanf("%d", &arr[i]);
+    mergeSort(arr, 0, n - 1);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+}
